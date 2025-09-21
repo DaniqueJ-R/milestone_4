d172129b75f56d3d27e32b9b634853bdeaade4ca
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-09-21 02:28:44 +0100

    Complete Index HTML

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,5 @@
-console.log("Script file loaded")
+console.log("Test script file loaded")
+
+// popup from bootsraps docs
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
