d8932ccdc7f2b063bc6b94b14affa31e3c75f39f
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-10-16 18:36:56 +0100

    updated and completed payments wehooks

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
-    path('success/<str:payment_number>/', views.payment_success, name="success"),
     path('donations/', views.make_payment, name="donations"),
+    path('wh/', webhook, name='webhook'),
+    path('success/<str:payment_number>/', views.payment_success, name="success"),
 ]
 
-# path('stripe/webhook/', stripe_webhook, name='stripe_webhook'),
